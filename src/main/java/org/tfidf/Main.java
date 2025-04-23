7bfb08edcbba3e4af1ea0667b8dc310236b153b0
IsraaXx <saramohamed2315@gmail.com>
2025-04-23 17:57:30 +0200

    update main

--- src/main/java/org/tfidf/Main.java
+++ src/main/java/org/tfidf/Main.java
@@ -1,7 +1,17 @@
 package org.tfidf;
+import org.tfidf.crawler.WebCrawler;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        final int MAX_PAGES = 10;
+
+        WebCrawler crawler = new WebCrawler();
+
+        System.out.println("Seed 1");
+        crawler.crawl("https://en.wikipedia.org/wiki/Pharaoh", MAX_PAGES);
+
+        System.out.println("\nSeed 2");
+        crawler.crawl("https://en.wikipedia.org/wiki/List_of_pharaohs", MAX_PAGES);
+
     }
 }
